0515b3e684a0dd23c4db4037017ba2f0eff0a376
James Marcil <james@jamesmarcil.com>
2018-04-29 15:19:51 -0400

    Implementation of code for Chapter 3

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 mod vec3;
+mod ray;
 
 use vec3::Vec3;
+use ray::Ray;
+
+fn get_color(r:Ray) -> Vec3 {
+    let unit_direction = r.direction().unit();
+    let t = 0.5 * (unit_direction.y() + 1.0);
+
+    Vec3::make(1.0, 1.0, 1.0) * (1.0 - t) + Vec3::make(0.5, 0.7, 1.0) * t
+}
 
 fn main() {
     let nx: i32 = 200;
@@ -10,13 +19,23 @@ fn main() {
     println!("{} {}", nx, ny);
     println!("255");
 
+    let lower_left_corner = Vec3::make(-2.0, -1.0, -1.0);
+    let horizontal = Vec3::make(4.0, 0.0, 0.0);
+    let vertical = Vec3::make(0.0, 2.0, 0.0);
+    let origin = Vec3::new();
+
     for j in (0..ny).rev() {
         for i in 0..nx {
-            let v = Vec3::make(i as f32 / nx as f32, j as f32 / ny as f32, 0.2);
+            let u = i as f32 / nx as f32;
+            let v = j as f32 / ny as f32;
 
-            let ir = (255.99 * v.r()) as u8;
-            let ig = (255.99 * v.g()) as u8;
-            let ib = (255.99 * v.b()) as u8;
+            let r = Ray::make(origin, lower_left_corner + horizontal * u + vertical * v);
+
+            let color = get_color(r);
+
+            let ir = (255.99 * color.r()) as u8;
+            let ig = (255.99 * color.g()) as u8;
+            let ib = (255.99 * color.b()) as u8;
             println!("{} {} {}", ir, ig, ib);
         }
     }
---
+++ src/ray.rs
@@ -0,0 +1,29 @@
+use vec3::Vec3;
+
+#[derive(Debug, Clone, Copy, Default)]
+pub struct Ray {
+    origin: Vec3,
+    direction: Vec3,
+}
+
+impl Ray {
+    pub fn new() -> Ray {
+        Ray::make(Vec3::new(), Vec3::new())
+    }
+
+    pub fn make(origin: Vec3, direction: Vec3) -> Ray {
+        Ray { origin, direction }
+    }
+
+    pub fn origin(&self) -> Vec3 {
+        self.origin
+    }
+
+    pub fn direction(&self) -> Vec3 {
+        self.direction
+    }
+
+    pub fn point_at_parameter(&self, t:f32) -> Vec3 {
+        self.origin + self.direction * t
+    }
+}
