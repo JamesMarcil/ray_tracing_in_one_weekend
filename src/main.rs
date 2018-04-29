9c3030edaec712f2101ff109f394284aafda44c0
James Marcil <james@jamesmarcil.com>
2018-04-29 14:40:37 -0400

    Adding code for Chapter 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    let nx: i32 = 200;
+    let ny: i32 = 100;
+
+    println!("P3");
+    println!("{} {}", nx, ny);
+    println!("255");
+
+    for j in (0..ny).rev() {
+        for i in 0..nx {
+            let r: f32 = i as f32 / nx as f32;
+            let g: f32 = j as f32 / ny as f32;
+            let b: f32 = 0.2;
+
+            let ir = (255.99 * r) as u8;
+            let ig = (255.99 * g) as u8;
+            let ib = (255.99 * b) as u8;
+            println!("{} {} {}", ir, ig, ib);
+        }
+    }
 }
